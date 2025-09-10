2b22fe71f150528360c9f46fba14e3674b0b586b
KevinW2027 <kevin.wang2027@gbwl.org>
2025-09-10 13:41:29 +0000

    edited main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("hello world")
+print("hello world")
 
-
-if __name__ == "__main__":
-    main()
+# Variables store data
+variable_1 = 64 #integer
+my_name = "KevinW" #str
+wallet = 7.77 #float
+raining_status  = bool(1) #bool
+if raining_status == True:
+    print(1)
